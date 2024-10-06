5d9d6c1b9db7c021582bb7be3a62f14c4cce0581
Ayush Aryan Singh <129523895+AyushGlitch@users.noreply.github.com>
2024-10-06 13:17:50 +0000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
-    return (
-        <>
-			<div className="flex h-screen justify-center items-center">
-				HomePage
+	return (
+		<>
+			<div className="flex h-screen justify-center items-center text-3xl font-semibold">
+				CodeBox Product Page
 			</div>
-        </>
-    );
+		</>
+	);
 }
